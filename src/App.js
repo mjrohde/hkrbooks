6da977763d2687395e03b649f2ec96b3cd44af17
Mattusio <60032522+Mattusio@users.noreply.github.com>
2022-02-15 19:08:23 +0100

    Trying to make this thing work

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/hkrbooks" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
