6c7c54673d8dce6b519bf0d6e2291118a7063256
Mattusio <60032522+Mattusio@users.noreply.github.com>
2022-11-12 16:20:30 +0100

    Price

--- src/Homepage.js
+++ src/Homepage.js
@@ -20,7 +20,7 @@ function Homepage() {
 
   const book = "003-Agentene og hemmeligheten";
 
-  const price = 299;
+  const price = 300;
 
   function calculateTotal(quantity) {
     setQuantity(quantity);
